f429c486a7146652d29eafc5f7b309314fb4df53
Steven Quispe <steven.quispe@epn.edu.ec>
2023-06-22 11:00:02 -0500

    Intents && List View

--- 01-Android/app/src/main/java/com/example/movilessoftware2023a/BlistView.kt
+++ 01-Android/app/src/main/java/com/example/movilessoftware2023a/BlistView.kt
@@ -2,10 +2,75 @@ package com.example.movilessoftware2023a
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ContextMenu
+import android.view.MenuItem
+import android.view.View
+import android.widget.Adapter
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.ListView
 
 class BlistView : AppCompatActivity() {
+    val arreglo = BbaseDatosMemoria.arregloBEntrenador
+    var idItemSeleccionado = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_blist_view)
+        //Adaptador
+        val listView = findViewById<ListView>(R.id.lv_list_view)
+        val adaptador = ArrayAdapter(
+            this,   //Contexto
+            android.R.layout.simple_list_item_1,
+            arreglo
+        )
+        listView.adapter = adaptador
+        adaptador.notifyDataSetChanged()
+        //Referencia al botón para añadir
+        val botonAnadirListView = findViewById<Button>(R.id.btn_añadir_list_view)
+        botonAnadirListView.setOnClickListener {
+            anadirEntrenador(adaptador)
+        }
+        registerForContextMenu(listView)
     }
+
+    override fun onCreateContextMenu(
+        menu: ContextMenu?,
+        v: View?,
+        menuInfo: ContextMenu.ContextMenuInfo?
+    ) {
+        super.onCreateContextMenu(menu, v, menuInfo)
+        //Llenar opciones de menu
+        val inflater = menuInflater
+        inflater.inflate(R.menu.menu, menu)
+        //Obtener el id del ArrayList seleccionado
+        val info = menuInfo as AdapterView.AdapterContextMenuInfo
+        val id =  info.position
+        idItemSeleccionado = id
+    }
+
+    override fun onContextItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId){
+            R.id.mi_editar->{
+                return true
+            }
+            R.id.mi_eliminar->{
+                return true
+            }
+            else -> super.onContextItemSelected(item)
+        }
+    }
+
+    //Funcion para ñadir elementos al arreglo y mostrarlos en pantalla
+    fun anadirEntrenador(
+        adaptador: ArrayAdapter<BEntrenador>
+    ){
+        arreglo.add(
+            BEntrenador(4, "Steven", "Descripcion")
+        )
+        adaptador.notifyDataSetChanged()
+    }
+
+
 }
--- 01-Android/app/src/main/java/com/example/movilessoftware2023a/MainActivity.kt
+++ 01-Android/app/src/main/java/com/example/movilessoftware2023a/MainActivity.kt
@@ -1,11 +1,29 @@
 package com.example.movilessoftware2023a
 
+import android.app.Activity
 import  android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import androidx.activity.result.contract.ActivityResultContracts
 
 class MainActivity : AppCompatActivity() {
+
+    val callbackContenidoIntentExlicito =
+        registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult()
+        ){
+            result ->
+            if(result.resultCode == Activity.RESULT_OK){
+                if( result.data != null){
+                    val data = result.data
+                    "${data?.getStringExtra("nombreModificado")}"
+                }
+            }
+        }
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -31,5 +49,18 @@ class MainActivity : AppCompatActivity() {
         val intent = Intent(this, clase)
         startActivity(intent)
         //this.startActivity()
+    }
+
+    fun abrirActividadConParametros(
+        clase: Class<*>
+    ){
+        val intentExplicito = Intent(this, clase)
+        //Enviar parametros (aceptamos primitivas)
+        intentExplicito.putExtra("nombre", "Steven")
+        intentExplicito.putExtra("apellido", "Quispe")
+        intentExplicito.putExtra("edad", "22")
+        //envio de intent con respuesta
+        callbackContenidoIntentExlicito
+            .launch(intentExplicito)
     }
 }

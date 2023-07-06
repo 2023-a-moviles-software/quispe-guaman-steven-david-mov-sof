af4abc16f0d4c2d2f719389e8ef4906488fe70f4
Steven Quispe <steven.quispe@epn.edu.ec>
2023-07-06 10:53:07 -0500

    RecyclerView

--- 01-Android/app/src/main/java/com/example/movilessoftware2023a/MainActivity.kt
+++ 01-Android/app/src/main/java/com/example/movilessoftware2023a/MainActivity.kt
@@ -31,6 +31,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //Base de datos SQLite
+        EBaseDeDatos.tablaEntrenador = ESqliteHelperEntrenador(this)
+
+
 
         val botonCicloVida= findViewById<Button>(
             R.id.btn_ciclo_vida
